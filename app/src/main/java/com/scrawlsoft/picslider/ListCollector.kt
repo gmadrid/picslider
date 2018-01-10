4a43dcfbd1f51e7283cef6f056d049eb4df12b9f
George Madrid <gmadrid@gmail.com>
2018-01-10 10:35:02 -0500

    BROKEN: but at least ListCollector seems to work.

--- app/src/main/java/com/scrawlsoft/picslider/ListCollector.kt
+++ app/src/main/java/com/scrawlsoft/picslider/ListCollector.kt
@@ -4,13 +4,13 @@ import io.reactivex.Observable
 import io.reactivex.Observable.just
 import io.reactivex.Single
 import io.reactivex.rxkotlin.Observables
+import io.reactivex.rxkotlin.subscribeBy
 
 /**
  * Exports a List<FeedlyEntry> and its count.
  * Manages getting more entries if required.
  */
 class ListCollector(private val imageService: ImageService,
-                    continuationRequest: Observable<Continuation>,
                     categoryName: Observable<String> = just("Porn")) {
 
     // We request categories one time when we start up, then we cache it for all future ops.
@@ -27,29 +27,34 @@ class ListCollector(private val imageService: ImageService,
             .replay(1)
             .autoConnect()
 
-    private val entryIdsResp: Observable<ImageService.EntryIdsResponse> = category.flatMap { cat ->
-        imageService.getEntryIdsForCategory(cat.id, NoContinuationToken)
-                .toObservable().concatMap { resp ->
-            if (resp.continuation == NoContinuationToken) {
-                println("FIRST ONE")
-                Observable.just(resp)
-            } else {
-                println("SECOND ONE")
-                Observable.just(resp).concatWith {
-                    imageService.getEntryIdsForCategory(cat.id, resp.continuation).toObservable()
+    private fun downloadPagedResponse(category: CategoryId, continuation: Continuation)
+            : Observable<ImageService.EntryIdsResponse> {
+        return imageService.getEntryIdsForCategory(category, continuation).toObservable()
+                .concatMap { response ->
+                    if (response.continuation == NoContinuationToken) {
+                        just(response)
+                    } else {
+                        just(response).concatWith(
+                                imageService.getEntryIdsForCategory(category, response.continuation)
+                                        .toObservable())
+                    }
                 }
-            }
-        }
     }
-            .doOnNext { println("NEXT happened") }
-            .doOnComplete { println("COMPLETE happened") }
 
-    val entries: Observable<List<ImageService.Entry>> = entryIdsResp
-            .concatMap { resp ->
-                println("GOT A RESP: $resp")
-                imageService.getEntriesForIds(resp.ids).toObservable() }
-//            .toList()
-//            .map { it.flatten() }
-//            .toObservable()
-//            .share()
+    private val entryIdsResp: Observable<ImageService.EntryIdsResponse> =
+            category.flatMap { category ->
+                downloadPagedResponse(category.id, NoContinuationToken)
+            }
+
+    val entries: MutableList<ImageService.Entry> = mutableListOf()
+
+    init {
+        entryIdsResp
+                .concatMap { resp ->
+                    imageService.getEntriesForIds(resp.ids).toObservable()
+                }
+                .subscribeBy { newEntries ->
+                    entries.addAll(newEntries)
+                }
+    }
 }
--- app/src/test/java/com/scrawlsoft/picslider/ListCollectorTest.kt
+++ app/src/test/java/com/scrawlsoft/picslider/ListCollectorTest.kt
@@ -2,11 +2,8 @@
 
 package com.scrawlsoft.picslider
 
-import io.reactivex.Observable
 import io.reactivex.Single
-import io.reactivex.observers.TestObserver
 import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.subjects.PublishSubject
 import junit.framework.Assert.*
 import org.junit.Test
 import java.net.URL
@@ -30,19 +27,19 @@ class ListCollectorTest {
 
         // Entries returned on a first call
         var entriesMap = hashMapOf(
-                simpleEntryPair("entryOne"),
-                simpleEntryPair("entryTwo"),
-                simpleEntryPair("entryThree"),
-                simpleEntryPair("entryFour")
+                simpleEntryPair("entry1"),
+                simpleEntryPair("entry2"),
+                simpleEntryPair("entry3"),
+                simpleEntryPair("entry4")
         )
         // Entries returned on a call with a continuation string of "continuing"
         var expectedContinuation = Continuation.fromString("EXPECTEDCONTINUATION")
         private var continuingMap = hashMapOf(
-                simpleEntryPair("continuingFive"),
-                simpleEntryPair("continuingSix"),
-                simpleEntryPair("continuingSeven"),
-                simpleEntryPair("continuingEight"),
-                simpleEntryPair("continuingNine")
+                simpleEntryPair("continuing5"),
+                simpleEntryPair("continuing6"),
+                simpleEntryPair("continuing7"),
+                simpleEntryPair("continuing8"),
+                simpleEntryPair("continuing9")
         )
 
 
@@ -51,16 +48,14 @@ class ListCollectorTest {
 
         override fun getEntryIdsForCategory(categoryId: CategoryId, continuation: Continuation)
                 : Single<ImageService.EntryIdsResponse> {
-            println("THE CONT: $continuation")
             val result = when (continuation) {
                 NoContinuationToken ->
-                    ImageService.EntryIdsResponse(nextContinuation, entriesMap.keys.toList())
+                    ImageService.EntryIdsResponse(nextContinuation, entriesMap.keys.sorted().toList())
                 else -> {
                     nextContinuation = NoContinuationToken
-                    ImageService.EntryIdsResponse(NoContinuationToken, continuingMap.keys.toList())
+                    ImageService.EntryIdsResponse(NoContinuationToken, continuingMap.keys.sorted().toList())
                 }
             }
-            println("RESULT FROM SERVICE: $result")
 
             return Single.create<ImageService.EntryIdsResponse> {
                 it.onSuccess(result)
@@ -69,77 +64,72 @@ class ListCollectorTest {
 
         override fun getEntriesForIds(entryIds: List<EntryId>): Single<List<ImageService.Entry>> =
                 Single.create<List<ImageService.Entry>> {
-                    it.onSuccess(entryIds.mapNotNull { entryId -> entriesMap[entryId] })
+                    it.onSuccess(entryIds.mapNotNull { entryId ->
+                        (entriesMap[entryId] ?: continuingMap[entryId])
+                    })
                 }
     }
 
-    @Test
-    fun `list collector with missing category`() {
-        val fakeService = FakeService()
-        fakeService.categoryResponse = fakeService.categoryResponse.filter { it.name != "Porn" }
-        val collector = ListCollector(fakeService, Observable.empty())
+// TODO: get this working again.
+//    @Test
+//    fun `list collector with missing category`() {
+//        val fakeService = FakeService()
+//        fakeService.categoryResponse = fakeService.categoryResponse.filter { it.name != "Porn" }
+//        val collector = ListCollector(fakeService)
+//
+//        collector.entries.subscribeBy(
+//                onError = { e ->
+//                    val s = "Category 'Porn' not found."
+//                    assertTrue("At least one of the exceptions should be from ListCollector",
+//                            e.message == s)
+//                },
+//                onComplete = { fail("expected error, not completion") },
+//                onNext = { fail("expected error, not result") })
+//    }
 
-        collector.entries.subscribeBy(
-                onError = { e ->
-                    val s = "Category 'Porn' not found."
-                    assertTrue("At least one of the exceptions should be from ListCollector",
-                            e.message == s)
-                },
-                onComplete = { fail("expected error, not completion") },
-                onNext = { fail("expected error, not result") })
-    }
-
-    @Test
-    fun `list collector with empty list`() {
-        val fakeService = FakeService()
-        fakeService.entriesMap = hashMapOf()
-        val collector = ListCollector(fakeService, Observable.empty())
-
-        collector.entries
-                .subscribeBy { entries ->
-                    assertTrue(entries.isEmpty())
-                }
-    }
-
-    @Test
-    fun `list collector with first list`() {
-        val fakeService = FakeService()
-        val collector = ListCollector(fakeService, Observable.empty())
-
-        collector.entries
-                .subscribeBy { entries ->
-                    assertEquals(4, entries.size)
-                }
-    }
-
-    class MyTestObserver : TestObserver<Triple<List<ImageService.Entry>, Boolean, Continuation>>() {
-        lateinit var entries: List<ImageService.Entry>
-        var hasContinuation: Boolean? = null
-        lateinit var continuation: Continuation
-
-        override fun onNext(t: Triple<List<ImageService.Entry>, Boolean, Continuation>) {
-            println("In onNext: $t")
-            val (entriesIn, hasContinuationIn, continuationIn) = t
-            super.onNext(t)
-            entries = entriesIn
-            hasContinuation = hasContinuationIn
-            continuation = continuationIn
-        }
-
-        override fun onComplete() {
-            println("COMPLETED")
-        }
-    }
+//    @Test
+//    fun `list collector with empty list`() {
+//        val fakeService = FakeService()
+//        fakeService.entriesMap = hashMapOf()
+//        val collector = ListCollector(fakeService)
+//
+//        collector.entries
+//                .subscribeBy { entries ->
+//                    assertTrue(entries.isEmpty())
+//                }
+//    }
+//
+//    @Test
+//    fun `list collector with first list`() {
+//        val fakeService = FakeService()
+//        val collector = ListCollector(fakeService)
+//
+//        collector.entries
+//                .subscribeBy { entries ->
+//                    assertEquals(4, entries.size)
+//                }
+//    }
+//
+//    @Test
+//    fun `list collector with second list appended`() {
+//        val fakeService = FakeService()
+//        fakeService.nextContinuation = fakeService.expectedContinuation
+//        val collector = ListCollector(fakeService)
+//
+//        val allEntries: MutableList<ImageService.Entry> = mutableListOf()
+//        collector.entries.subscribeBy(onNext = { entries ->
+//            allEntries.addAll(entries)
+//        })
+//        assertEquals(9, allEntries.size)
+//    }
 
     @Test
-    fun `list collector with second list appended`() {
+    fun foobar() {
         val fakeService = FakeService()
         fakeService.nextContinuation = fakeService.expectedContinuation
-        val collector = ListCollector(fakeService, Observable.empty())
+        val collector = ListCollector(fakeService)
 
-        collector.entries.subscribeBy { entries ->
-            //assertEquals(9, entries.size)
-        }
+        assertEquals(9, collector.entries.size)
     }
 
 //    @Test
